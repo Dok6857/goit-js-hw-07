2b4386c11577d24106da4161f2a8d3c05696e3f6
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-12-01 20:23:56 +0200

    Initial commit

---
+++ JS/task-3.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value.concat(str);
+  }
+
+  padStart(str) {
+    this.#value = this.#value.padStart(this.#value.length + 1, str);
+  }
+
+  padBoth(str) {
+    this.#value = this.#value.padStart(this.#value.length + 1, str).concat(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
