b6e297653e869ccb7ea4975d3db11ae2ca035f6b
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-12-02 20:38:41 +0200

    task 5 started

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,12 @@
+'use strict';
+const changeBtn = document.querySelector('.change-color'),
+  span = document.querySelector('.color'),
+  body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeBtn.addEventListener('click', getRandomHexColor);
